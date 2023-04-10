e0aff96c54f03b2a25976650604b391a87fcda01
Guilherme Amorim <guilhermedeamorim098@gmail.com>
2023-04-10 17:30:01 -0300

    Inicializando React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src='https://media.istockphoto.com/id/92236499/pt/foto/chimpanz%C3%A9-jogando-com-um-computador-port%C3%A1til.jpg?s=1024x1024&w=is&k=20&c=_HXj4PTBZY0WKmf6Owb324tgKgAizYzNoIETggImgys=' alt='Macaquinho codando' className='img'/>
+        </>
+    );
+}
+
+export default Home;
